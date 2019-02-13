0545c03eefe2927f95a1329277cb4561b13555dd
dayne <dayne@scoir.com>
2019-02-13 09:45:30 -0500

    allow user to specify the searchable string

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const findJsFiles = require('./lib/find-js-files')
 const analyze = require('./lib/analyze')
 const formatter = require('./lib/formatter')
 
+const debug = require('debug')('ast')
+
 const ProgressBar = require('progress')
 const argv = require('yargs')
     .option('format', {
@@ -9,26 +11,31 @@ const argv = require('yargs')
         choices: ['json', 'table'],
         default: 'json'
     })
+    .option('repos', {
+        describe: 'repos to look at',
+        demandOption: true,
+        type: 'array',
+    })
     .help()
     .argv
 
-const targetDirectories = argv._
+const searchTarget = argv._
 const files = []
 
-targetDirectories.forEach(dir => {
+argv.repos.forEach(dir => {
   const fileCountBefore = files.length
-  console.log(`processing files in ${dir}`)
+  debug(`processing files in ${dir}`)
   findJsFiles(dir, files, __dirname)
   const fileCountAfter = files.length
-  console.log(` found ${fileCountAfter - fileCountBefore} files`)
+  debug(` found ${fileCountAfter - fileCountBefore} files`)
 })
 
-console.log(`${files.length} total files`)
+debug(`${files.length} total files`)
 
 const bar = new ProgressBar(' processing [:bar] :current/:total', { total: files.length })
 
 const finalResult = files.reduce((result, file) => {
-  analyze(file, result)
+  analyze(file, result, searchTarget)
   bar.tick()
   return result
 }, {})
